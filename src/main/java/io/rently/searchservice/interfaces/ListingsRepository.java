daac2950ff2c9c7f9f93c57819d3ae37977e54a4
chandle greff <unconfigured@null.spigotmc.org>
2022-04-13 23:53:01 +0200

    Added some missing mongodb queries

--- src/main/java/io/rently/searchservice/interfaces/ListingsRepository.java
+++ src/main/java/io/rently/searchservice/interfaces/ListingsRepository.java
@@ -8,17 +8,25 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.List;
 
 public interface ListingsRepository extends MongoRepository<Listing, String> {
-    String ADDRESS_QUERY = "{ $or: [ {'address.country': ?1}, {'address.city': ?2}, {'address.zip': ?3} ] }";
-    String SEARCH_QUERY = "{ $text: { $search: ?0 } }";
+
+    @Query("{ $text: { $search: ?0 } }")
+    List<Listing> queryAny(String query, Pageable pageable);
 
     @Query("{ 'address.location': { $near: {$maxDistance: ?2, $geometry: { type: 'Point', coordinates: [?0, ?1]} } } }")
-    List<Listing> findNearByGeoCode(Double lon, Double lat, Integer range, Pageable pageable);
+    List<Listing> queryAnyNearbyGeoCode(Double lon, Double lat, Integer range, Pageable pageable);
 
-    @Query(
-            "{ $and: [" +
+    @Query("{ $and: [" +
             "{ $text: { $search: ?0 } }, " +
-            "{ $or: [{'address.country': ?1}, {'address.city': ?2}, {'address.zip': ?3}] }" +
-            "] }"
-    )
-    List<Listing> findByAddress(String query, String country, String city, String zip, Pageable pageable);
+            "{ 'address.location': { $near: {$maxDistance: ?3, $geometry: { type: 'Point', coordinates: [?1, ?2]} } } }" +
+            "] }")
+    List<Listing> queryNearbyGeoCode(String query, Double lon, Double lat, Integer range, Pageable pageable);
+
+    @Query("{ $or: [ {'address.country': ?1}, {'address.city': ?2}, {'address.zip': ?3} ] }")
+    List<Listing> queryAnyAtAddress(String country, String city, String zip, Pageable pageable);
+
+    @Query("{ $and: [" +
+            "{ $text: { $search: ?0 } }, " +
+            "{ $or: [ {'address.country': ?1}, {'address.city': ?2}, {'address.zip': ?3} ] }" +
+            "] }")
+    List<Listing> queryAtAddress(String query, String country, String city, String zip, Pageable pageable);
 }
