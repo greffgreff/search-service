3e5b03002a7f2649dc33282e0e334c462aa68232
chandle greff <unconfigured@null.spigotmc.org>
2022-04-15 23:52:11 +0200

    Fixed lat lon on queries

--- src/main/java/io/rently/searchservice/interfaces/ListingsRepository.java
+++ src/main/java/io/rently/searchservice/interfaces/ListingsRepository.java
@@ -3,6 +3,7 @@ package io.rently.searchservice.interfaces;
 import io.rently.searchservice.dtos.Listing;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.geo.GeoPage;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -17,14 +18,14 @@ public interface ListingsRepository extends MongoRepository<Listing, String> {
     @Query("{ $text: { $search: ?0 } }")
     Page<Listing> query(String query, Pageable pageable);
 
-    @Query("{ 'address.location': { $near: {$maxDistance: ?2, $geometry: { type: 'Point', coordinates: [?0, ?1]} } } }")
-    Page<Listing> queryAnyNearbyGeoCode(Double lon, Double lat, Integer range, Pageable pageable);
+    @Query("{ 'address.location': { $near: {$maxDistance: ?2, $geometry: {type: 'Point', coordinates: [?1, ?0]} } } }")
+    Page<Listing> queryAnyNearbyGeoCode(Double lat, Double lon, Integer range, Pageable pageable);
 
     @Query("{ $and: [" +
             "{ $or: [ {'name': /?0/i }, {'desc': /?0/i } ] }, " +
-            "{ 'address.location': { $near: {$maxDistance: ?3, $geometry: { type: 'Point', coordinates: [?1, ?2] } } } }" +
+            "{ 'address.location': { $near: {$maxDistance: ?3, $geometry: {type: 'Point', coordinates: [?2, ?1]} } } }" +
             "] }")
-    Page<Listing> queryNearbyGeoCode(String query, Double lon, Double lat, Integer range, Pageable pageable);
+    Page<Listing> queryNearbyGeoCode(String query, Double lat, Double lon, Integer range, Pageable pageable);
 
     @Query("{ $or: [ {'address.country': ?1}, {'address.city': ?2}, {'address.zip': ?3} ] }")
     Page<Listing> queryAnyAtAddress(String country, String city, String zip, Pageable pageable);
