fa17ccd07341088a7fbd8ab5b9251d61c70c77f6
chandle greff <unconfigured@null.spigotmc.org>
2022-04-15 19:48:09 +0200

    Made repo queries return Page class

--- src/main/java/io/rently/searchservice/interfaces/ListingsRepository.java
+++ src/main/java/io/rently/searchservice/interfaces/ListingsRepository.java
@@ -18,20 +18,20 @@ public interface ListingsRepository extends MongoRepository<Listing, String> {
     Page<Listing> query(String query, Pageable pageable);
 
     @Query("{ 'address.location': { $near: {$maxDistance: ?2, $geometry: { type: 'Point', coordinates: [?0, ?1]} } } }")
-    List<Listing> queryAnyNearbyGeoCode(Double lon, Double lat, Integer range, Pageable pageable);
+    Page<Listing> queryAnyNearbyGeoCode(Double lon, Double lat, Integer range, Pageable pageable);
 
     @Query("{ $and: [" +
             "{ $or: [ {'name': /?0/i }, {'desc': /?0/i } ] }, " +
             "{ 'address.location': { $near: {$maxDistance: ?3, $geometry: { type: 'Point', coordinates: [?1, ?2] } } } }" +
             "] }")
-    List<Listing> queryNearbyGeoCode(String query, Double lon, Double lat, Integer range, Pageable pageable);
+    Page<Listing> queryNearbyGeoCode(String query, Double lon, Double lat, Integer range, Pageable pageable);
 
     @Query("{ $or: [ {'address.country': ?1}, {'address.city': ?2}, {'address.zip': ?3} ] }")
-    List<Listing> queryAnyAtAddress(String country, String city, String zip, Pageable pageable);
+    Page<Listing> queryAnyAtAddress(String country, String city, String zip, Pageable pageable);
 
     @Query("{ $and: [" +
             "{ $text: { $search: ?0 } }, " +
             "{ $or: [ {'address.country': ?1}, {'address.city': ?2}, {'address.zip': ?3} ] }" +
             "] }")
-    List<Listing> queryAtAddress(String query, String country, String city, String zip, Pageable pageable);
+    Page<Listing> queryAtAddress(String query, String country, String city, String zip, Pageable pageable);
 }
