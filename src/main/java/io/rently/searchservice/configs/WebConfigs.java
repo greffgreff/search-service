685f9829e8ab32440442160233809e07c265dfbe
chandle greff <unconfigured@null.spigotmc.org>
2022-04-16 01:03:38 +0200

    Added Cors

---
+++ src/main/java/io/rently/searchservice/configs/WebConfigs.java
@@ -0,0 +1,14 @@
+package io.rently.searchservice.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigs implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
+    }
+}
