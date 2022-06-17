5f282b3fff29b812bcd7cfc4fadf52c1a3b0853e
chandle greff <unconfigured@null.spigotmc.org>
2022-06-17 16:19:18 +0200

    Renamed to AppConfi`

--- src/main/java/io/rently/searchservice/configs/AppConfigs.java
+++ src/main/java/io/rently/searchservice/configs/AppConfigs.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfigs implements WebMvcConfigurer {
+public class AppConfigs implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
